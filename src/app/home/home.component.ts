4b3f3d049e5d6fe77ba026de8b988200307f7e82
Nain <335210@cognizant.com>
2018-12-19 16:05:26 +0530

    Obesrvables Start

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('third');
+      }, 6000)
+    });
+    myObservable.subscribe(
+      (data:string) => { console.log(data)},
+      (error:string) => { console.log(error)},
+      () => { console.log('completed')}
+    )
+  }
+
+}
